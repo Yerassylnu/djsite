53f4184e9d67bc3cd6c3ead4a7658748ae03f887
Yerassylnu <programist.solo.company@gmail.com>
2023-11-11 22:14:19 +0600

    finished video #12 custom template tags

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -11,10 +11,8 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
 
     context = {'posts': posts,
-               'cats': cats,
                'menu': menu,
                'title': 'Главная страница',
                'cat_selected': 0,
@@ -41,13 +39,11 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {'posts': posts,
-               'cats': cats,
                'menu': menu,
                'title': 'Отображение по рубрикам',
                'cat_selected': cat_id,
